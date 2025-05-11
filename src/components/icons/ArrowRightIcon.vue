9162bb04cb6da15c236ab02f965105e26928645f
daiwanxing <david377099119@gmail.com>
2025-05-11 16:59:59 +0800

    chore: add README

--- src/components/icons/ArrowRightIcon.vue
+++ src/components/icons/ArrowRightIcon.vue
@@ -1,20 +1,21 @@
 <script setup lang="ts">
 defineOptions({
-    name: 'ArrowRightIcon'
-})
+  name: "ArrowRightIcon",
+});
 </script>
 
 <template>
-  <svg xmlns="http://www.w3.org/2000/svg"
-       width="24"
-       height="24"
-       viewBox="0 0 24 24"
-       fill="none"
-       stroke="currentColor"
-       stroke-width="3"
-       stroke-linecap="round"
-       stroke-linejoin="round"
-       class="arrow-right-icon"
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="3"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    class="arrow-right-icon"
   >
     <path d="m12 5 7 7-7 7" />
   </svg>
